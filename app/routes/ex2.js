8ccf75b2a2ce3f04ea42ddd5271dd9e520df826d
Lucas Levillain <21904015@etu.unicaen.fr>
2021-01-26 17:08:57 +0100

    td1ajoutapp

---
+++ app/routes/ex2.js
@@ -0,0 +1,16 @@
+import Route from '@ember/routing/route';
+
+
+export default class Ex2Route extends Route {
+  /*actions: {
+    clear() {
+      console.log("camarchepas");
+    }
+    save() {
+      console.log("camarchepas");
+    }
+    update() {
+      console.log("camarchepas");
+    }
+  }*/ // <--marche pas
+}
