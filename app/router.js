dd35003d8fd476bd336aed8b5698722525823231
Lucas Levillain <21904015@etu.unicaen.fr>
2021-02-02 17:04:36 +0100

    finTD2

--- app/router.js
+++ app/router.js
@@ -1,9 +1,13 @@
 import EmberRouter from '@ember/routing/router';
-import config from 'td1/config/environment';
+import config from 'td2/config/environment';
 
 export default class Router extends EmberRouter {
   location = config.locationType;
   rootURL = config.rootURL;
 }
 
-Router.map(function () {});
+Router.map(function () {
+  this.route('TD2');
+  this.route('ex2');
+  this.route('Ex2');
+});
---
+++ app/routes/ex2.js
@@ -0,0 +1,24 @@
+import Route from '@ember/routing/route';
+
+export default class Ex2Route extends Route {
+
+}
+
+/*export default Ember.Component.extend({
+  prix : 0.00,
+  actions: {
+    webdev() {
+      this.prix = ((this.prix)+300.00);
+    },
+    design() {
+      this.prix = ((this.prix)+400.00);
+    },
+    integration() {
+      this.prix = ((this.prix)+250.00);
+    },
+    formation() {
+      this.prix = ((this.prix)+220.00);
+    }
+  }
+});
+*/
